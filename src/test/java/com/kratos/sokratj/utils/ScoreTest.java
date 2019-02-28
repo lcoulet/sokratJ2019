ba1ee985133728459715e9b1f39302554387fbbb
Serriere Guillaume <guillaume.serriere@kratoscomms.com>
2019-02-28 19:29:22 +0100

    Fix test
    Signed-off-by: Serriere Guillaume <guillaume.serriere@kratoscomms.com>

--- src/test/java/com/kratos/sokratj/utils/ScoreTest.java
+++ src/test/java/com/kratos/sokratj/utils/ScoreTest.java
@@ -1,6 +1,5 @@
 package com.kratos.sokratj.utils;
 
-import com.google.common.collect.ImmutableList;
 import com.kratos.sokratj.model.ImmutablePhoto;
 import com.kratos.sokratj.model.ImmutableSlide;
 import com.kratos.sokratj.model.Photo;
@@ -8,7 +7,7 @@ import org.junit.Test;
 
 import java.util.Arrays;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class ScoreTest {
     private static ImmutableSlide getSlide(final Photo... photos) {
@@ -18,13 +17,13 @@ public class ScoreTest {
     @Test
     public void testScore() {
         ImmutablePhoto photo1 = ImmutablePhoto.builder().addAllTags(Arrays.asList("tag1", "tag2", "tag3"))
-                .id(1).build();
+                                              .id(1).isVertical(false).build();
         ImmutablePhoto photo2 = ImmutablePhoto.builder().addAllTags(Arrays.asList("tag2", "tag3", "tag4"))
-                                              .id(1).build();
+                                              .id(1).isVertical(false).build();
         ImmutablePhoto photo3 = ImmutablePhoto.builder().addAllTags(Arrays.asList("tag5"))
-                                              .id(1).build();
+                                              .id(1).isVertical(false).build();
         ImmutablePhoto photo4 = ImmutablePhoto.builder().addAllTags(Arrays.asList("tag6", "tag3", "tag4"))
-                                              .id(1).build();
+                                              .id(1).isVertical(false).build();
 
         assertEquals(1, Score.computeScore(getSlide(photo1), getSlide(photo2)));
         assertEquals(0, Score.computeScore(getSlide(photo1), getSlide(photo3)));
